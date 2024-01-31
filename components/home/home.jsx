5e6c75d7f0eb40b1e8a319486ad43bd7f95c2425
Dev4dig <developer4digital@gmail.com>
2024-01-31 17:06:42 +0400

    first build with ull homepage nextjs

---
+++ components/home/home.jsx
@@ -0,0 +1,229 @@
+import React from "react";
+// import "./home.scss";
+const HomeContent = () => {
+    return (
+        <>
+            {/* <!-- hero --> */}
+            <div class="hero">
+                <div class="container">
+                    <div class="nav">
+                        <div class="logo">
+                            <img src="/images/logo2.png" alt="" />
+                        </div>
+                        <div class="menu">
+                            <ul class="menu-list">
+                                <li class="link active">
+                                    <a href="">Home</a>
+                                </li>
+                                <li class="link">
+                                    <a href="">Book</a>
+                                </li>
+                                <li class="link">
+                                    <a href="">Contact</a>
+                                </li>
+                                <li class="link">
+                                    <a href="">About</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="content">
+                        <div class="text">
+                            <div class="social">
+                                <span class="icon">
+                                    <img src="/images/fb.svg" alt="" />
+                                </span>
+                                <span class="icon">
+                                    <img src="/images/insta.svg" alt="" />
+                                </span>
+                            </div>
+                            <p>
+                                We take photos as
+                                <br />
+                                a return ticket to a <br />
+                                moment otherwise gone
+                            </p>
+                            <button>Book Now</button>
+                        </div>
+                        <div class="view-more">
+                            <a href="">View More</a>
+                            <img src="/images/to-down.svg" alt="" />
+                            <span></span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            {/* <!-- beauty --> */}
+            <div class="beauty">
+                <div class="container">
+                    <div class="content-col">
+                        <div class="side-border">
+                            <div class="top-bar"></div>
+                            <h2>BEAUTY</h2>
+                            <p>
+                                m officiis quam neque. Fuga reprehenderit
+                                reiciendis repellendus? m officiis quam neque.
+                                Fuga reprehenderit reiciendis repellendus?
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            {/* <!-- potrait --> */}
+            <div class="potrait">
+                <div class="container">
+                    <div class="content-col">
+                        <div class="side-border">
+                            <div class="top-bar"></div>
+                            <h2>PORTRAIT</h2>
+                            <p>
+                                m officiis quam neque. Fuga reprehenderit
+                                reiciendis repellendus? m officiis quam neque.
+                                Fuga reprehenderit reiciendis repellendus?
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            {/* <!-- fashion --> */}
+            <div class="fashion">
+                <div class="container">
+                    <div class="content-col">
+                        <div class="side-border">
+                            <div class="top-bar"></div>
+                            <h2>FASHION</h2>
+                            <p>
+                                m officiis quam neque. Fuga reprehenderit
+                                reiciendis repellendus? m officiis quam neque.
+                                Fuga reprehenderit reiciendis repellendus?
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            {/* <!-- meeting --> */}
+            <div class="meeting">
+                <div class="container">
+                    <div class="content-col">
+                        <div class="side-border">
+                            <div class="top-bar"></div>
+                            <h2>MEETING</h2>
+                            <p>
+                                m officiis quam neque. Fuga reprehenderit
+                                reiciendis repellendus? m officiis quam neque.
+                                Fuga reprehenderit reiciendis repellendus?
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            {/* {/* <!-- people --> */}
+            <div class="people">
+                <div class="container">
+                    <div class="content-col">
+                        <div class="side-border">
+                            <div class="top-bar"></div>
+                            <h2>PEOPLE</h2>
+                            <p>
+                                m officiis quam neque. Fuga reprehenderit
+                                reiciendis repellendus? m officiis quam neque.
+                                Fuga reprehenderit reiciendis repellendus?
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            {/* <!-- product --> */}
+            <div class="product">
+                <div class="container">
+                    <div class="content-col">
+                        <div class="side-border">
+                            <div class="top-bar"></div>
+                            <h2>PRODUCT</h2>
+                            <p>
+                                m officiis quam neque. Fuga reprehenderit
+                                reiciendis repellendus? m officiis quam neque.
+                                Fuga reprehenderit reiciendis repellendus?
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            {/* <!-- couple --> */}
+            <div class="couple">
+                <div class="container">
+                    <div class="content-col">
+                        <div class="side-border">
+                            <div class="top-bar"></div>
+                            <h2>COUPLE</h2>
+                            <p>
+                                m officiis quam neque. Fuga reprehenderit
+                                reiciendis repellendus? m officiis quam neque.
+                                Fuga reprehenderit reiciendis repellendus?
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            {/* <!-- nature --> */}
+            <div class="nature">
+                <div class="container">
+                    <div class="content-col">
+                        <div class="side-border">
+                            <div class="top-bar"></div>
+                            <h2>NATURE</h2>
+                            <p>
+                                m officiis quam neque. Fuga reprehenderit
+                                reiciendis repellendus? m officiis quam neque.
+                                Fuga reprehenderit reiciendis repellendus?
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            {/* <!-- birthdays --> */}
+            <div class="birthdays">
+                <div class="container">
+                    <div class="content-col">
+                        <div class="side-border">
+                            <div class="top-bar"></div>
+                            <h2>BIRTHDAYS</h2>
+                            <p>
+                                m officiis quam neque. Fuga reprehenderit
+                                reiciendis repellendus? m officiis quam neque.
+                                Fuga reprehenderit reiciendis repellendus?
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="footer">
+                <div class="content">
+                    <div class="back-to-top">
+                        <img src="/images/to-up.svg" alt="" />
+                        <a href="">Back to top</a>
+                    </div>
+                    <div class="footer-content">
+                        <div class="first footer-col">
+                            <h2>25</h2>
+                            <p>Years experience of freelance photography</p>
+                        </div>
+                        <div class="second footer-col">
+                            <h2>49</h2>
+                            <p>Projects worked on and counting</p>
+                        </div>
+                        <div class="third footer-col">
+                            <h2>36</h2>
+                            <p>
+                                Stable client currently trust us and are booked
+                            </p>
+                        </div>
+                    </div>
+                    <button>Book Now</button>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default HomeContent;
--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+    reactStrictMode: true,
 };
 
 export default nextConfig;
+
+// // next.config.js
+// const withSass = require("@zeit/next-sass");
+
+// module.exports = withSass({
+//     /* additional configuration options here */
+// });
