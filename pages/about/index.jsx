a54b05f33ba6144bb5ce42d02c5f38494f05bdeb
Dev4dig <developer4digital@gmail.com>
2024-02-21 01:28:57 +0500

    spcoa;

--- pages/about/index.jsx
+++ pages/about/index.jsx
@@ -3,36 +3,61 @@ import Nav from "@/components/nav/nav";
 import React from "react";
 
 const About = () => {
-    return <div className="page-about  min-h-[100vh] bg-black h-fit w-full">
-    <Nav page={"about"}/>
-    <MobileNav page={"about"}  />
-    <div className="content flex min-content h-full w-full  ">
-        <div className="left hidden lg:flex items-start justify-center min-h-[500px] p-2 pt-[80px] w-2/5">
-            <img className="h-[900px]" src={"/images/cali.png"} />
-        </div>
-        <div className="right p-5  w-full lg:w-3/5  flex flex-col items-end gap-10 lg:pt-[80px] lg:px-[5.563rem] ">
-            <div className=" w-full lg:w-[570px] lg:flex flex-col gap-7">
-                <h2 className="text-white text-[4rem] font-normal font-sans]">
-                    About
-                </h2>
+    return (
+        <div className="page-about  min-h-[100vh] bg-black h-fit w-full">
+            <Nav page={"about"} />
+            <MobileNav page={"about"} />
+            <div className="content flex min-content h-full w-full  ">
+                <div className="left hidden lg:flex  flex-col items-center gap-3 justify-center min-h-[500px] p-2 pt-[80px] w-2/5">
+                    <img
+                        className="h-[800px] w-[280]"
+                        src={"/images/cali.png"}
+                    />
+                    <div className="flex w-[280px]  gap-4 ">
+                        <div className="social w-[280px] justify-between flex gap-3 ">
+                            <span className="icon">
+                                <img src="/images/behance.svg" alt="" />
+                            </span>
+                            <span className="icon">
+                                <img src="/images/fb.svg" alt="" />
+                            </span>
+                            <span className="icon">
+                                <img src="/images/insta.svg" alt="" />
+                            </span>
+                        </div>
+                    </div>
+                </div>
 
-                <p className="text-white font-normal text-[1.25rem]">
-                    Lorem ipsum dolor sit amet, consectetur 
-                    adipiscing elit. Non nunc eleifend id pulvinar. 
-                    Id pretium feugiat dignissim quisque. 
-                    Duis purus tellus duis nunc rhoncus, ultrices 
-                    id in ipsum. Eget sit tempor habitant id turpis 
-                    imperdiet ut. 
-                </p>
+                <div className="right p-5  w-full lg:w-3/5  flex flex-col items-end gap-10 lg:pt-[80px] lg:px-[5.563rem] ">
+                    <div className=" w-full lg:w-[570px] lg:flex flex-col gap-7">
+                        <h2 className="text-white text-[4rem] font-normal font-sans]">
+                            About
+                        </h2>
 
-                <p className="mt-5 text-white font-normal text-[1.25rem]">
-                If you are looking to make enquiries, 
-kindly <a className="text-[#CC0909]" href="/contact">contact</a> us or make your booking <a href="/contact" className="text-[#CC0909]" >here</a>.
-                </p>
+                        <p className="text-white font-normal text-[1.25rem]">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing
+                            elit. Non nunc eleifend id pulvinar. Id pretium
+                            feugiat dignissim quisque. Duis purus tellus duis
+                            nunc rhoncus, ultrices id in ipsum. Eget sit tempor
+                            habitant id turpis imperdiet ut.
+                        </p>
+
+                        <p className="mt-5 text-white font-normal text-[1.25rem]">
+                            If you are looking to make enquiries, kindly{" "}
+                            <a className="text-[#CC0909]" href="/contact">
+                                contact
+                            </a>{" "}
+                            us or make your booking{" "}
+                            <a href="/contact" className="text-[#CC0909]">
+                                here
+                            </a>
+                            .
+                        </p>
+                    </div>
+                </div>
             </div>
         </div>
-    </div>
-</div>;
+    );
 };
 
 export default About;
--- pages/contact/index.jsx
+++ pages/contact/index.jsx
@@ -7,12 +7,28 @@ import MobileNav from "@/components/mobile-nav/mobile-nav";
 const Contact = () => {
     return (
         <>
-            <div className="page-contact  bg-black min-h-[100vh] w-full">
-                <Nav page={"contact"}/>
-                <MobileNav page={"contact"}  />
-                <div className="content flex h-full w-full  ">
-                    <div className="left hidden lg:flex items-start justify-center h-[500px] p-2 pt-[80px] w-2/5">
-                        <img className="h-[900px]" src={"/images/cali.png"} />
+            <div className="page-contact  bg-black min-fit w-full">
+                <Nav page={"contact"} />
+                <MobileNav page={"contact"} />
+                <div className="content flex min-fit w-full  ">
+                    <div className="  left hidden lg:flex gap-4 items-center flex-col justify-start min-fit p-2 pt-[80px] w-2/5">
+                        <img
+                            className="h-[800px] w-[280px]"
+                            src={"/images/cali.png"}
+                        />
+                        <div className="flex w-[280px]  gap-4 ">
+                            <div className="social w-[280px] justify-between flex gap-3 ">
+                                <span className="icon">
+                                    <img src="/images/behance.svg" alt="" />
+                                </span>
+                                <span className="icon">
+                                    <img src="/images/fb.svg" alt="" />
+                                </span>
+                                <span className="icon">
+                                    <img src="/images/insta.svg" alt="" />
+                                </span>
+                            </div>
+                        </div>
                     </div>
                     <div className="right p-5  w-full lg:w-3/5  flex flex-col items-end gap-10 pt-[80px] lg:px-[5.563rem] ">
                         <div className=" w-full lg:w-[570px] lg:flex flex-col gap-7">
@@ -23,15 +39,30 @@ const Contact = () => {
                             <div className="links flex flex-col text-white gap-5 mt-[5rem] w-full lg:w-[570px]">
                                 <div className="phone flex justify-end gap-3">
                                     <p>+92-334-4567890</p>
-                                    <span>icon</span>
+                                    <span>
+                                        <img
+                                            className="w-[20px] h-[20px]"
+                                            src="/images/tel.svg"
+                                        />
+                                    </span>
                                 </div>
                                 <div className="address flex justify-end gap-3">
                                     <p>Lane 3, Gulshan-e-Hadeed, Lahore</p>
-                                    <span>icon</span>
+                                    <span>
+                                        <img
+                                            className="w-[20px] h-[20px]"
+                                            src="/images/loc.svg"
+                                        />
+                                    </span>
                                 </div>
                                 <div className="mail flex justify-end gap-3">
                                     <p>alistudiophotography@gmail.com</p>
-                                    <span>icon</span>
+                                    <span>
+                                        <img
+                                            className="w-[20px] h-[20px]"
+                                            src="/images/mail.svg"
+                                        />
+                                    </span>
                                 </div>
                             </div>
                         </div>
